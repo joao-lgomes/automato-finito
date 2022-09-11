3e79f5549d45ef724d168535af9293b17369fa16
gkazioka <gugakazioka@gmail.com>
2022-09-11 18:36:19 -0300

    :bug: fix automaton d

--- automato-finito/src/main/java/com/mycompany/automato/finito/constants/Automatons.java
+++ automato-finito/src/main/java/com/mycompany/automato/finito/constants/Automatons.java
@@ -48,7 +48,7 @@ public final class Automatons {
         states.add(new TransitionState(2, A, 1));
         states.add(new TransitionState(2, B, 2));
 
-        automatons.add(new AutomatonManager('a', "começa com a e termina com b", states, Arrays.asList(2)));
+        automatons.add(new AutomatonManager('a', "Começa com a e termina com b", states, Arrays.asList(2)));
         
         states = new ArrayList();
 
@@ -61,7 +61,7 @@ public final class Automatons {
         states.add(new TransitionState(2, B, 0));
         states.add(new TransitionState(3, AeB, 3));
         
-        automatons.add(new AutomatonManager('b', "possui aaa como subcadeia", states, Arrays.asList(3)));
+        automatons.add(new AutomatonManager('b', "Possui aaa como subcadeia", states, Arrays.asList(3)));
         
         states = new ArrayList();
         
@@ -83,7 +83,7 @@ public final class Automatons {
         states.add(new TransitionState(7, B, 5));
         states.add(new TransitionState(8, AeB, 8));
         
-        automatons.add(new AutomatonManager('c', "possui baba como prefixo e abab como sufixo", states, Arrays.asList(7)));
+        automatons.add(new AutomatonManager('c', "Possui baba como prefixo e abab como sufixo", states, Arrays.asList(7)));
         
         states = new ArrayList();
         
@@ -95,17 +95,21 @@ public final class Automatons {
         states.add(new TransitionState(2, B, 3));
         states.add(new TransitionState(3, A, 4));
         states.add(new TransitionState(3, B, 0));
-        states.add(new TransitionState(4, A, 4));
-        states.add(new TransitionState(4, B, 5));
-        states.add(new TransitionState(5, A, 6));
-        states.add(new TransitionState(5, B, 4));
-        states.add(new TransitionState(6, A, 4));
+        states.add(new TransitionState(4, A, 5));
+        states.add(new TransitionState(4, B, 6));
+        states.add(new TransitionState(5, A, 5));
+        states.add(new TransitionState(5, B, 7));
+        states.add(new TransitionState(6, A, 10));
         states.add(new TransitionState(6, B, 7));
         states.add(new TransitionState(7, A, 8));
-        states.add(new TransitionState(7, B, 4));
-        states.add(new TransitionState(8, AeB, 8));
+        states.add(new TransitionState(7, B, 5));
+        states.add(new TransitionState(8, A, 5));
+        states.add(new TransitionState(8, B, 9));
+        states.add(new TransitionState(9, A, 10));
+        states.add(new TransitionState(9, B, 5));
+        states.add(new TransitionState(10, AeB, 10));
         
-        automatons.add(new AutomatonManager('d', "possui no máximo uma única ocorrência da cadeia baba", states, Arrays.asList(0,1,2,3,4,5,6,7)));
+        automatons.add(new AutomatonManager('d', "Possui no máximo uma única ocorrência da cadeia baba", states, Arrays.asList(0,1,2,3,4,5,6,7,8,9)));
         
         states = new ArrayList();
         
@@ -119,7 +123,7 @@ public final class Automatons {
         states.add(new TransitionState(3, B, 1));
         states.add(new TransitionState(4, AeB, 4));
         
-        automatons.add(new AutomatonManager('e', "não possui ocorrência da cadeia baba", states, Arrays.asList(0,1,2,3)));
+        automatons.add(new AutomatonManager('e', "Não possui ocorrência da cadeia baba", states, Arrays.asList(0,1,2,3)));
           
         states = new ArrayList();
         
@@ -135,7 +139,7 @@ public final class Automatons {
         states.add(new TransitionState(4, B, 3));
         states.add(new TransitionState(5, AeB, 6));
         
-        automatons.add(new AutomatonManager('f', "possui ocorrência par de a seguida por ocorrência ímpar de b", states, Arrays.asList(3)));
+        automatons.add(new AutomatonManager('f', "Possui ocorrência par de a seguida por ocorrência ímpar de b", states, Arrays.asList(3)));
         
         states = new ArrayList();
         
@@ -152,7 +156,7 @@ public final class Automatons {
         states.add(new TransitionState(4, AaZsemB, 4));
         states.add(new TransitionState(5, AaZ, 5));
         
-        automatons.add(new AutomatonManager('g', "começa com a e possui ocorrência par de a 's ou começa com b e possui ocorrência impar de b 's", states, Arrays.asList(2,3)));
+        automatons.add(new AutomatonManager('g', "Começa com a e possui ocorrência par de a 's ou começa com b e possui ocorrência impar de b 's", states, Arrays.asList(2,3)));
        
         states = new ArrayList();
         
@@ -165,7 +169,7 @@ public final class Automatons {
         states.add(new TransitionState(2, Odd, 1));
         states.add(new TransitionState(3, AllNumbers, 3));
         
-        automatons.add(new AutomatonManager('h', "inicia-se com 0 e a soma de todos os seus dígitos é par, ou inicia-se com 1 e a soma de todos os seus dígitos é impar}", states, Arrays.asList(1)));
+        automatons.add(new AutomatonManager('h', "Inicia-se com 0 e a soma de todos os seus dígitos é par, ou inicia-se com 1 e a soma de todos os seus dígitos é impar", states, Arrays.asList(1)));
         
         states = new ArrayList();
         
@@ -174,7 +178,7 @@ public final class Automatons {
         states.add(new TransitionState(1, NumbersAndLetters, 1));
         states.add(new TransitionState(2, NumbersAndLetters, 2));
         
-        automatons.add(new AutomatonManager('i', "inicia-se com uma letra, possuindo a seguir qualquer combinação de letras e dígitos", states, Arrays.asList(1)));
+        automatons.add(new AutomatonManager('i', "Inicia-se com uma letra, possuindo a seguir qualquer combinação de letras e dígitos", states, Arrays.asList(1)));
         
         states = new ArrayList();
         
@@ -206,7 +210,7 @@ public final class Automatons {
         states.add(new TransitionState(10, AllNumbers, 10));
         states.add(new TransitionState(10, Symbols, 2));
         
-        automatons.add(new AutomatonManager('j', "é um número inteiro, um número decimal ou um número representado em notação científica", states, Arrays.asList(1, 5, 8, 10)));
+        automatons.add(new AutomatonManager('j', "É um número inteiro, um número decimal ou um número representado em notação científica", states, Arrays.asList(1, 5, 8, 10)));
         
         return automatons;
     }
